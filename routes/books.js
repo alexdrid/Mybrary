b53d5bd2d064e7b248c6e79a520933c7edff67f6
= <=>
2020-05-18 19:18:48 +0300

    Finish Project

--- routes/books.js
+++ routes/books.js
@@ -12,10 +12,10 @@ router.get('/', async (req, res) => {
     query = query.regex('title', new RegExp(req.query.title, 'i'));
   }
   if (req.query.publishedBefore != null && req.query.publishedBefore != '') {
-    query = query.lte('publishDate', req.query.publishedBefore);
+    query = query.lte('publicationDate', req.query.publishedBefore);
   }
   if (req.query.publishedAfter != null && req.query.publishedAfter != '') {
-    query = query.gte('publishDate', req.query.publishedAfter);
+    query = query.gte('publicationDate', req.query.publishedAfter);
   }
   try {
     const books = await query.exec();
